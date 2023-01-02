babbd5c800e5c179c484d78978157f9e9dd2cc81
Jedrzej Nowak <jnowak3@sii.pl>
2023-01-02 11:05:19 +0100

    homework from lesson 14

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello world');
+const { ApolloServer, gql } = require("apollo-server");
+const typeDefs = gql`
+type Query {
+  greeting:String
+  interestingUrls: [String]
+  firstName: String
+  email: String
+  pets:[String]
+}
+`;
+
+const data = {
+  greeting:"Hello world",
+  interestingUrls:["www.google.pl","www.google.com"],
+  firstName: "John",
+  email: "john@example.com",
+  pets: ["Mittens", "Doggo", "Birb"]
+};
+const server = new ApolloServer({typeDefs , rootValue : data});
+server.listen({port:4000}).then((result)=> console.log(result))
